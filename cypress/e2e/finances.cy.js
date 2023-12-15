7dd68c34534d5dea48588a4f427bdee9787b5e9d
Sabrina Bueno <bina_fbueno@hotmail.com>
2023-12-15 09:34:14 -0300

    Assertions

--- cypress/e2e/finances.cy.js
+++ cypress/e2e/finances.cy.js
@@ -6,14 +6,12 @@ describe('Transações', () => {
         cy.get('#description').type("Freela")
         cy.get('#amount').type("500")
         cy.get('#date').type("2023-12-14") // yyyy-mm-dd      
+        
         cy.contains('button', 'Salvar').click()
 
-        cy.contains("Nova Transação").click()
-        cy.get('#description').type("Cinema")
-        cy.get('#amount').type("-100")
-        cy.get('#date').type("2023-12-15") // yyyy-mm-dd      
-        cy.contains('button', 'Salvar').click()
+        cy.get("tbody tr td.description").should("have.text", "Freela")
 
+        
     });
     
 });
