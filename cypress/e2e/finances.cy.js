2a614d0ac51c1401e67d884856154317bcd63cc3
Sabrina Bueno <bina_fbueno@hotmail.com>
2023-12-15 10:58:19 -0300

    conhecendo os Hooks

--- cypress/e2e/finances.cy.js
+++ cypress/e2e/finances.cy.js
@@ -1,17 +1,25 @@
 
 describe('Transações', () => {
+
+// hooks -> executar antes ou depois / de cada ou de todos os teste
+// before - para executar antes de todos os testes 
+// after - executar o código 1x depois de todos os testes 
+// beforeEach - exercutar um determinado código antes de cada teste
+// afterEach - exercutar um determinado código depois de cada teste
+
+beforeEach(() => {
+    cy.visit("https://devfinance-agilizei.netlify.app/#")
+});
+
     it('Cadastrar uma entrada', () => {
-        cy.visit("https://devfinance-agilizei.netlify.app/#")
-        
+                
         criarTransacao("Freela", 250)
         
-
         cy.get("tbody tr td.description").should("have.text", "Freela")
     });
 
     it('Cadastrar uma saída', () => {
-        cy.visit("https://devfinance-agilizei.netlify.app/#")
-    
+            
         criarTransacao("Cinema", -45)
     
         cy.get("tbody tr td.description").should("have.text", "Cinema")
